0e24a05aad259fa1312b4d98201ef9ce54bfe776
karthik <karthik@gmail.com>
2024-03-26 11:24:57 +0530

    commit message

---
+++ build/precache-manifest.296178726404c2a75ff9fb07b62fce41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806abe2804c3ff8bb1db180f0535f667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "819432c88e1618a8013c",
+    "url": "/static/css/main.9b9b8edb.chunk.css"
+  },
+  {
+    "revision": "56718828970c9551cd9f",
+    "url": "/static/js/2.cd6d0286.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cd6d0286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "819432c88e1618a8013c",
+    "url": "/static/js/main.fa1753a8.chunk.js"
+  },
+  {
+    "revision": "1d34c1ef83c210c405ce",
+    "url": "/static/js/runtime-main.e9946a1e.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/static/media/city.e1698332.jpg"
+  }
+]);
